ca5d645f25737cb517f4b64cd99bc9affc31fc1a
Moritaka <842788849@qq.com>
2022-03-03 15:55:25 +0800

    functions about message send and receive

---
+++ utils.py
@@ -0,0 +1,20 @@
+import pickle
+import socket
+import struct
+
+
+def send_msg(sock, msg):
+    msg_pickle = pickle.dumps(msg)
+    sock.sendall(struct.pack(">I", len(msg_pickle)))
+    sock.sendall(msg_pickle)
+    print(msg[0], 'sent to', sock.getpeername())
+
+def recv_msg(sock, expect_msg_type=None):
+    msg_len = struct.unpack(">I", sock.recv(4))[0]
+    msg = sock.recv(msg_len, socket.MSG_WAITALL)
+    msg = pickle.loads(msg)
+    print(msg[0], 'received from', sock.getpeername())
+
+    if (expect_msg_type is not None) and (msg[0] != expect_msg_type):
+        raise Exception("Expected " + expect_msg_type + " but received " + msg[0])
+    return msg
